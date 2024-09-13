730f82ac0679650ae7f915a36ac67f89150061fe
George Benjamin-Schonberger <george.benjamin@gmail.com>
2024-09-13 23:12:56 +0300

    [feat] base update script added

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+###
+# AdeoTEK dotfiles update
+###
+
+# Init
+if [[ -d "${0%/*}" ]]; then
+  CDIR="${0%/*}/_scripts/core"
+else
+  CDIR="$PWD/_scripts/core";
+fi
+
+## Includes
+source "$CDIR/_helpers.sh"
+
+## Startup
+cecho "blue" "Starting dotfiles update..."
+
+# Main
+case $CURRENT_OS_ID in
+  arch)
+    sudo pacman -Suy --noconfirm
+    yay -Suy --noconfirmation
+  ;;
+  debian|ubuntu)
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt autoremove
+  ;;
+  *)
+    cecho "red" "ERROR: Unsupported OS: $CURRENT_OS_ID!"
+    exit 1
+  ;;
+esac
+
+if [[ -x "$(command -v brew)" ]]; then
+  brew upgrade
+fi
+
+if [[ -x "$(command -v npm)" ]]; then
+  npm install -g npm
+fi
+
+## End
+cecho "blue" "DONE!"
+
