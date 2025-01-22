0b005de71ce305992391525077529e1178f12206
George Benjamin-Schonberger <george.benjamin@gmail.com>
2025-01-23 01:08:38 +0200

    [fix:nvim] nvim config fixes (#30)

--- nvim/.config/nvim/lua/configs/adeotek_v2/plugins/telescope.lua
+++ nvim/.config/nvim/lua/configs/adeotek_v2/plugins/telescope.lua
@@ -10,11 +10,16 @@ return {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make',
       },
+      { 
+        'nvim-telescope/telescope-live-grep-args.nvim',
+        version = '^1.0.0' 
+      },
     },
     config = function()
-      require('telescope').setup({
+      local telescope = require('telescope')
+      telescope.setup({
         defaults = {
-          file_ignore_patterns = { "node_modules", ".git/", ".cache", "tldr" },
+          file_ignore_patterns = { 'node_modules', '.git', '.cache', 'tldr' },
           mappings = {
             i = {
               ['<C-u>'] = false,
@@ -22,8 +27,14 @@ return {
             },
           },
         },
+        pickers = {
+          find_files = {
+            hidden = true
+          }
+        }
       })
-      require('telescope').load_extension('fzf')
+      telescope.load_extension('fzf')
+      telescope.load_extension('live_grep_args')
     end,
   },
 }
