376acd3075114bedf4ef605e520d697112bdf6c2
George Benjamin-Schonberger <george.benjamin@gmail.com>
2025-09-01 23:54:51 +0300

    [fix:scripts] zed install script fix (#47)

--- _scripts/core/zed-install.sh
+++ _scripts/core/zed-install.sh
@@ -17,14 +17,7 @@ fi
 
 # Install
 case $CURRENT_OS_ID in
-  arch)
-    if [ flatpak --version >/dev/null 2>&1 ]; then
-      install_package "zed" "zed --version" "flatpak install -y dev.zed.Zed"
-    else
-      install_package "zed" "zed --version"
-    fi
-  ;;
-  debian|ubuntu)
+  arch|debian|ubuntu|pop|fedora)
     cecho "cyan" "Installing [zed]..."
     if [ zed --version >/dev/null 2>&1 ]; then
       decho "yellow" "Package already installed. Updating it..."
@@ -38,16 +31,8 @@ case $CURRENT_OS_ID in
       cecho "yellow" "DRY-RUN: curl -f https://zed.dev/install.sh | sh"
     fi
   ;;
-  pop)
-    if [ flatpak --version >/dev/null 2>&1 ]; then
-      install_package "zed" "zed --version" "flatpak install -y dev.zed.Zed"
-    else
-      install_package "zed" "zed --version"
-    fi
-  ;;
   *)
     cecho "red" "Unsupported OS: $CURRENT_OS_ID"
     exit 1
   ;;
 esac
-
