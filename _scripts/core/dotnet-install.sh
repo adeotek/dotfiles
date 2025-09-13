51f14287aac98ec9de12eff9acf670bae6ddd5ea
George Benjamin-Schonberger <george.benjamin@gmail.com>
2025-09-13 15:38:04 +0300

    [fix:ms] dotnet and powershell install fixes (#52)
    * [fix:ms] dotnet and powershell install fixes
    
    * [fix:ms] dotnet and powershell install fixes

--- _scripts/core/dotnet-install.sh
+++ _scripts/core/dotnet-install.sh
@@ -62,41 +62,13 @@ else
           cecho "yellow" "DRY-RUN: chmod +x dotnet-install.sh"
           cecho "yellow" "DRY-RUN: ./dotnet-install.sh --channel $DOTNET_VERSION"
         fi
-      else   
-        if [ ! -f /etc/apt/sources.list.d/microsoft-prod.list ]; then
-          cecho "cyan" "Installing Microsoft APT source..."
-          if [ "$DRY_RUN" -ne "1" ]; then
-            decho "magenta" "wget https://packages.microsoft.com/config/debian/$CURRENT_OS_VER/packages-microsoft-prod.deb -O packages-microsoft-prod.deb"
-            wget https://packages.microsoft.com/config/debian/$CURRENT_OS_VER/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-            decho "magenta" "sudo dpkg -i packages-microsoft-prod.deb"
-            sudo dpkg -i packages-microsoft-prod.deb
-            decho "magenta" "rm packages-microsoft-prod.deb"
-            rm packages-microsoft-prod.deb
-            decho "magenta" "sudo apt-get update"
-            sudo apt-get update
-          else
-            cecho "yellow" "DRY-RUN: wget https://packages.microsoft.com/config/debian/$CURRENT_OS_VER/packages-microsoft-prod.deb -O packages-microsoft-prod.deb"
-            cecho "yellow" "DRY-RUN: sudo dpkg -i packages-microsoft-prod.deb"
-            cecho "yellow" "DRY-RUN: rm packages-microsoft-prod.deb"
-            cecho "yellow" "DRY-RUN: sudo apt-get update"
-          fi
-        fi
+      else
+        source "$CDIR/microsoft-repo-install.sh"
         install_package "dotnet-sdk-$DOTNET_VERSION" "dotnet --version"
       fi
       ;;
     ubuntu|pop)
-      if [[ "$CURRENT_OS_VER" != "24.10" ]]; then
-        if ! grep -q "^deb.*dotnet/backports" /etc/apt/sources.list.d/*.list 2>/dev/null; then
-          cecho "cyan" "Enabling dotnet backports Ubuntu feed..."
-          if [ "$DRY_RUN" -ne "1" ]; then
-            sudo add-apt-repository -y ppa:dotnet/backports
-            sudo apt-get update
-          else
-            cecho "yellow" "DRY-RUN: sudo add-apt-repository -y ppa:dotnet/backports"
-            cecho "yellow" "DRY-RUN: sudo apt-get update"
-          fi
-        fi
-      fi
+      source "$CDIR/microsoft-repo-install.sh"
       install_package "dotnet-sdk-$DOTNET_VERSION" "dotnet --version"
       ;;
     fedora|redhat|centos|almalinux)
--- bash/.config/bash/config.bash
+++ bash/.config/bash/config.bash
@@ -101,11 +101,6 @@ if [[ -x "$(command -v fzf)" ]]; then
   alias dpsfzf='docker ps -a | fzf --preview "docker inspect {1}"'
 fi
 
-# zoxide
-if $(command -v zoxide >/dev/null 2>&1); then
-  eval "$(zoxide init bash)"
-fi
-
 # yazi
 if $(command -v yazi >/dev/null 2>&1); then
   function yy() {
@@ -145,3 +140,8 @@ fi
 # if $(command -v starship >/dev/null 2>&1); then
 #   eval "$(starship init bash)"
 # fi
+
+# zoxide
+if $(command -v zoxide >/dev/null 2>&1); then
+  eval "$(zoxide init bash)"
+fi
