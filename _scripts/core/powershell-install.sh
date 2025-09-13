7a6d8e76cecf19387988d3e487881510e2e1c402
George Benjamin-Schonberger <george.benjamin@gmail.com>
2025-09-13 15:37:04 +0300

    [fix:ms] dotnet and powershell install fixes

--- _scripts/core/powershell-install.sh
+++ _scripts/core/powershell-install.sh
@@ -24,18 +24,22 @@ case $CURRENT_OS_ID in
     if [[ "$CURRENT_ARCH" == "aarch64" ]]; then
       cecho "yellow" "SKIPPED: not available on ARM-based systems."
     else
-      PWSH_PACKAGE_URL="$(curl -s https://api.github.com/repos/PowerShell/PowerShell/releases/latest | jq -r '.assets[] | select(.name | contains(".deb_amd64.deb")) | .browser_download_url')"
-      if [ "$DRY_RUN" -ne "1" ]; then
-        decho "magenta" "wget $PWSH_PACKAGE_URL -O /tmp/powershell.deb"
-        wget "$PWSH_PACKAGE_URL" -O /tmp/powershell.deb
-        decho "magenta" "sudo apt-get install /tmp/powershell.deb -y"
-        sudo apt-get install /tmp/powershell.deb -y
-        decho "magenta" "rm -f /tmp/powershell.deb"
-        rm -f /tmp/powershell.deb
+      if [ "$CURRENT_OS_ID" != "debian" ] || [ "$CURRENT_OS_VER" != "13" ]; then
+        cecho "yellow" "SKIPPED: not available yet on Debian 13 systems."
       else
-        cecho "yellow" "DRY-RUN: wget $PWSH_PACKAGE_URL -O /tmp/powershell.deb"
-        cecho "yellow" "DRY-RUN: sudo apt-get install /tmp/powershell.deb -y"
-        cecho "yellow" "DRY-RUN: rm -f /tmp/powershell.deb"
+        PWSH_PACKAGE_URL="$(curl -s https://api.github.com/repos/PowerShell/PowerShell/releases/latest | jq -r '.assets[] | select(.name | contains(".deb_amd64.deb")) | .browser_download_url')"
+        if [ "$DRY_RUN" -ne "1" ]; then
+          decho "magenta" "wget $PWSH_PACKAGE_URL -O /tmp/powershell.deb"
+          wget "$PWSH_PACKAGE_URL" -O /tmp/powershell.deb
+          decho "magenta" "sudo apt-get install /tmp/powershell.deb -y"
+          sudo apt-get install /tmp/powershell.deb -y
+          decho "magenta" "rm -f /tmp/powershell.deb"
+          rm -f /tmp/powershell.deb
+        else
+          cecho "yellow" "DRY-RUN: wget $PWSH_PACKAGE_URL -O /tmp/powershell.deb"
+          cecho "yellow" "DRY-RUN: sudo apt-get install /tmp/powershell.deb -y"
+          cecho "yellow" "DRY-RUN: rm -f /tmp/powershell.deb"
+        fi
       fi
     fi
     ;;
