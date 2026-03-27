4ec7ee99c838c1ab856b89ec5f1138bff81be107
George Benjamin-Schonberger <george.benjamin@gmail.com>
2026-03-28 01:11:45 +0200

    [feat:tmux] tmux enhancements and fixes (#70)

--- _scripts/core/tmux-setup.sh
+++ _scripts/core/tmux-setup.sh
@@ -19,14 +19,14 @@ fi
 source "$CDIR/tmux-install.sh"
 
 # Setup
-if [ ! -f $CDIR/../../tmux/.config/tmux/tmux.conf.local ]; then
+if [ ! -e "$CDIR/../../tmux/.config/tmux/tmux.conf.local" ]; then
   read -p "Please select tmux local config: light/full [l/F] " tmux_local_mode
   if [[ "$tmux_local_mode" == "l" ]]; then
     tmux_local_config="gbs.light"
   else
     tmux_local_config="gbs.full"
   fi
-  ln -sr $CDIR/../../tmux/.config/tmux/$tmux_local_config.tmux.conf.local $CDIR/../../tmux/.config/tmux/tmux.conf.local
+  ln -sr "$CDIR/../../tmux/.config/tmux/$tmux_local_config.tmux.conf.local" "$CDIR/../../tmux/.config/tmux/tmux.conf.local"
 fi
 
 stow_package "tmux" "" "$CURRENT_CONFIG_DIR/tmux"
