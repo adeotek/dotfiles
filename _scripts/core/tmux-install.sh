d1dfbed79e57055ac2c97dacd5335c911b0836e9
George Benjamin-Schonberger <george.benjamin@gmail.com>
2026-04-11 00:19:58 +0300

    [fix:config] tmux config fix (#74)
    * [fix:config] tmux config fix
    
    * [fix:config] tmux config fix

--- _scripts/core/tmux-install.sh
+++ _scripts/core/tmux-install.sh
@@ -18,3 +18,12 @@ fi
 # Install
 install_package "tmux" "tmux -V"
 
+# Install tmux-256color terminfo (needed for correct key sequences over SSH)
+case $CURRENT_OS_ID in
+  debian|ubuntu|pop)
+    install_package "ncurses-term" "infocmp tmux-256color"
+    ;;
+  arch|fedora|redhat)
+    install_package "ncurses" "infocmp tmux-256color"
+    ;;
+esac
